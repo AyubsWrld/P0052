73d9899a1fa12063d86810dfb9c4cc326a7e4eab
AyubsWrld <ayubfmohammed@gmail.com>
2026-01-25 15:23:25 -0700

    scope_exit ctor && type constraints layout

--- scope.hpp
+++ scope.hpp
@@ -2,15 +2,24 @@
 
 
 #include <type_traits>
+#include <concepts>
+#include <utility>
 namespace Atlas
 {
-    template<typename E>
+
+    /*
+        scope_exit is a general-purpose scope guard that calls its exit function when a scope is exited. The
+        class templates scope_fail and scope_success share the scope_exit interface, only the situation
+        when the exit function is called differs.
+    */
+    
+    template<typename EF>
     class scope_exit;
 
-    template<typename E>
+    template<typename EF>
     class scope_fail;
 
-    template<typename E>
+    template<typename EF>
     class scope_success;
 
     template<typename R, typename D>
@@ -19,4 +28,47 @@ namespace Atlas
     template<typename R, typename D, typename S=std::decay<R>>
     unique_resource<std::decay_t<R>, std::decay_t<D>> 
     make_unique_resource_checked(R&& r, const S& invalid, D&& d) noexcept;
+
+
+    /* NOTE:    If the exit function object of a scope_success or scope_exit object refers to a local variable
+                of the function where it is defined, e.g., as a lambda capturing the variable by reference, and that
+                variable is used as a return operand in that function, that variable might have already been returned
+                when the scope_guard’s destructor executes, calling the exit function. This can lead to surprising
+                behavior.
+     */
+    
+    template<typename EF> 
+    class scope_exit 
+    {
+    private:
+        EF      exit_function;                       // exposition only 
+        bool    execute_on_destruction{true};      // exposition only
+        
+        // int     uncaught_on_creation{uncaught_exceptions()};
+    public:
+
+        template<typename EFP>
+        requires std::is_nothrow_constructible_v<EF, EFP> || std::is_nothrow_constructible_v<EF, EFP&> 
+        explicit scope_exit(EFP&& Callable) noexcept
+            :
+                exit_function(std::forward<EFP>(Callable))
+        {}
+
+        explicit scope_exit(scope_exit&& rhs) noexcept;
+
+        ~scope_exit() noexcept; 
+
+        void release() noexcept;
+
+        scope_exit(const scope_exit& other)                 =   delete;
+        scope_exit& operator=(const scope_exit& other)      =   delete;
+        scope_exit& operator=(scope_exit&& other)           =   delete;
+
+       
+    };
+}
+
+
+namespace Atlas 
+{
 }
